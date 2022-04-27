f9ec99e92993b7d559c7055c7f536d85e19e7217
Alex C <pyps9804@gmail.com>
2022-04-27 23:05:07 +0800

    change HW2.py
    built basis function

--- HW2.py
+++ HW2.py
@@ -38,6 +38,36 @@ def CalMSE(data, prediction):
 
     return mean__squared_error
 
+def BasisFunc(data, O_1, O_2):
+    x1_max = max(data[:,0])
+    x1_min = min(data[:,0])
+    x2_max = max(data[:,1])
+    x2_min = min(data[:,1])
+    s1 = (x1_max-x1_min)/(O_1-1)
+    s2 = (x2_max-x2_min)/(O_2-1)
+    x1 = data[:,0]
+    x2 = data[:,1]
+    x3 = data[:,2]
+    data_row = data.shape[0]
+
+    output = np.zeros((data_row, O_1*O_2+2))
+    # k Goes from 1-25, 
+    # expands the data from 2+experience+bias to 25+experience+bias
+    for row in range(data_row):
+        for i in range(1,O_1+1,1):
+            for j in range(1,O_2+1,1):
+                k = O_2*(i-1)+j
+                mu_i = s1*(i-1)+x1_min
+                mu_j = s2*(j-1)+x2_min
+                phi_k = math.exp(-math.pow(x1[row]-mu_i, 2)/2*s1*s1-math.pow(x2[row]-mu_j,2)/2*s2*s2)
+                output[row][k-1] = phi_k
+        output[row][k+1-1] = x3[row]
+        output[row][k+2-1] = 1
+    print(output[:,26])
+
+
+    
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -50,18 +80,14 @@ def main():
     data_train = pd.read_csv('Training_set.csv', header=None).to_numpy()    # 300 train data (300, 4)
     data_test = pd.read_csv('Validation_set.csv', header=None).to_numpy()   # 100 test data (100, 4)
     data_test_feature = data_test[:, :3]                                    # Features of the dataset (100, 3)
-    data_test_label = data_test[:, 3]                                       # Labels of the dataset (100, )
-
-    # print(data_train, data_train.shape)
-    # print(data_test, data_test.shape)
-    # print(data_test_feature, data_test_feature.shape)
-    # print(data_test_label, data_test_label.shape)
+    data_test_label = data_test[:, 3]                   
 
     '''
     Prediction using :
     1. Baysian linear regression method (BLR)
     2. Maximum Likelihood method (ML)
     '''
+    BasisFunc(data_train, O_1, O_2)
     
     # predict_BLR = BLR(data_train, data_test_feature, O1=O_1, O2=O_2)
     # predict_MLR = MLR(data_train, data_test_feature, O1=O_1, O2=O_2)
@@ -71,3 +97,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
