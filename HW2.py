90c555624de0b7d2542b46c6083f6a9ab1115b92
Alex C <pyps9804@gmail.com>
2022-04-28 15:42:40 +0800

    change HW2.py
    finish ML and BLR functions

--- HW2.py
+++ HW2.py
@@ -3,6 +3,7 @@ NTHU EE Machine Learning HW2
 Author:      陳家斌
 Student ID:  110033534
 '''
+from asyncore import dispatcher_with_send
 import numpy as np
 import pandas as pd
 import math
@@ -15,7 +16,26 @@ def BLR(train_data, test_data_feature, O1=5, O2=5):  # remember to set best choi
     '''
     output: ndarray with size (length of test_data, )
     '''
+    cvt_train_data = BasisFunc(train_data, O1, O2)
 
+    # Compute the design matrix 
+    phi = cvt_train_data
+    
+    # posterior distribution over w given by p(w|t) = N(w|m_n,S_n)
+    alpha = 1
+    beta  = 1
+    S_n_inv = alpha*np.identity(phi.shape[1])+beta*np.matmul(np.transpose(phi),phi)
+    S_n = np.linalg.inv(S_n_inv)
+    m_n = beta*np.matmul(np.matmul(S_n,np.transpose(phi)),train_data[:,3])
+
+    # Predict test_data_feature
+    cvt_test_data = BasisFunc(test_data_feature, O1, O2)
+    y_BLRprediction = []
+    for data in range(cvt_test_data.shape[0]):
+        y = 0
+        for j in range(cvt_test_data.shape[1]):
+            y+=m_n[j]*cvt_test_data[data,j]
+        y_BLRprediction.append(y)
 
     return y_BLRprediction 
 
@@ -27,7 +47,26 @@ def MLR(train_data, test_data_feature, O1=5, O2=5):  # remember to set best choi
     '''
     cvt_train_data = BasisFunc(train_data, O1, O2)
 
+    # Compute the design matrix 
+    phi = cvt_train_data
 
+    # Generate maximum likelihood weight from solving the gradient
+    # w_ml = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(phi),phi)),np.transpose(phi)),train_data[:,3])
+
+    # Generate maximum likelihood weight with regularization (lambda term)
+    lam = .5
+    w_ml = np.matmul(np.matmul(np.linalg.inv(lam/2*np.identity(phi.shape[1])+np.matmul(np.transpose(phi),phi)),np.transpose(phi)),train_data[:,3])
+
+    # Predict test_data_feature
+    cvt_test_data = BasisFunc(test_data_feature, O1, O2)
+    y_MLRprediction = []
+    for data in range(cvt_test_data.shape[0]):
+        y = 0
+        for j in range(cvt_test_data.shape[1]):
+            y+=w_ml[j]*cvt_test_data[data,j]
+        y_MLRprediction.append(y)
+
+    # print(y_MLRprediction)
     return y_MLRprediction 
 
 
@@ -40,6 +79,7 @@ def CalMSE(data, prediction):
     return mean__squared_error
 
 def BasisFunc(data, O_1, O_2):
+    # Calculate parameters
     x1_max = max(data[:,0])
     x1_min = min(data[:,0])
     x2_max = max(data[:,1])
@@ -49,9 +89,17 @@ def BasisFunc(data, O_1, O_2):
     x1 = data[:,0]
     x2 = data[:,1]
     x3 = data[:,2]
+
+    # Normalize data x1 and x2
+    # x1 = Normalize(x1, x1_max, x1_min)
+    # x2 = Normalize(x2, x2_max, x2_min)
+    
+    # Obtain # of rows in the train data
     data_row = data.shape[0]
 
+    # Create output matrix
     output = np.zeros((data_row, O_1*O_2+2))
+
     # k Goes from 1-25, 
     # expands the data from 2+experience+bias to 25+experience+bias
     for row in range(data_row):
@@ -60,14 +108,16 @@ def BasisFunc(data, O_1, O_2):
                 k = O_2*(i-1)+j
                 mu_i = s1*(i-1)+x1_min
                 mu_j = s2*(j-1)+x2_min
-                phi_k = math.exp(-math.pow(x1[row]-mu_i, 2)/2*s1*s1-math.pow(x2[row]-mu_j,2)/2*s2*s2)
+                phi_k = math.exp(-math.pow(x1[row]-mu_i, 2)/2/s1/s1-math.pow(x2[row]-mu_j,2)/2/s2/s2)
                 output[row][k-1] = phi_k
         output[row][k+1-1] = x3[row]
         output[row][k+2-1] = 1
-    
+    # print(output, output.shape)
     return output
 
-
+def Normalize(data, max, min):
+    data = (data-min)/(max-min)
+    return data
     
 
 
@@ -89,12 +139,12 @@ def main():
     1. Baysian linear regression method (BLR)
     2. Maximum Likelihood method (ML)
     '''
-    BasisFunc(data_train, O_1, O_2)
-    
-    # predict_BLR = BLR(data_train, data_test_feature, O1=O_1, O2=O_2)
-    # predict_MLR = MLR(data_train, data_test_feature, O1=O_1, O2=O_2)
 
-    # print('MSE of BLR = {e1}, MSE of MLR= {e2}.'.format(e1=CalMSE(predict_BLR, data_test_label), e2=CalMSE(predict_MLR, data_test_label)))
+    predict_BLR = BLR(data_train, data_test_feature, O1=O_1, O2=O_2)
+    predict_MLR = MLR(data_train, data_test_feature, O1=O_1, O2=O_2)
+    # print('MSE of MLR= {e2}.'.format(e2=CalMSE(predict_MLR, data_test_label)))
+    # print('MSE of BLR = {e1}.'.format(e1=CalMSE(predict_BLR, data_test_label)))
+    print('MSE of BLR = {e1}, MSE of MLR= {e2}.'.format(e1=CalMSE(predict_BLR, data_test_label), e2=CalMSE(predict_MLR, data_test_label)))
 
 
 if __name__ == '__main__':
